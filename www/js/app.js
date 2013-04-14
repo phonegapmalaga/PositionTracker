43b02a6a09ba078fa5b215f0f9059c78d5deaa06
Alberto Bautista <alberto.bautista@me.com>
2013-04-14 19:43:13 +0200

    added handler for networks evetns

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,38 @@
 $.when(pgReady, jqmReady).then(function() {
-    alert("initiliaztion completed");
+
+    // variables
+    var trackingData = [],
+    watchId = null,
+    networkStatus = null,
+
+    // events handlers
+    checkNetworkStatus = function() {
+        console.log("checking network status");
+        var newStatus = navigator.network.connection.type;
+        if (newStatus == networkStatus) {
+            return;
+        }
+
+        if (newStatus == Connection.NONE) {
+            $("#network-button").text("No Internet Access")
+                .buttonMarkup({icon:"delete", theme:"e"})
+                .button("refresh");
+            console.log("app :: no connection");
+        } else {
+            $("#network-button").text("Internet Access Enabled")
+                .buttonMarkup({icon:"check", theme:"b"})
+                .button("refresh");
+            console.log("app :: connected");
+        }
+    };
+
+    // bindings
+    
+
+
+    $("#about").on("pageshow", checkNetworkStatus);
+    $(document).on("online", checkNetworkStatus);
+    $(document).on("offline", checkNetworkStatus);
+
 });
 
