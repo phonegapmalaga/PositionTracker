9f92442695ebed56b9adabf7249b53a08a12718a
Alberto Bautista <alberto.bautista@me.com>
2013-04-15 07:40:32 +0200

    added saple data

--- www/js/app.js
+++ www/js/app.js
@@ -58,6 +58,14 @@ $.when(pgReady, jqmReady).then(function() {
         $("#tracking-field").show();
         $("#tracking-status").html(
             "Stopped tracking workout: <strong>"  + trackID + "</strong>");
+    },
+
+    clearLocalData = function() {
+        window.localStorage.clear();
+    },
+
+    loadSampleData = function() {
+        window.localStorage.setItem('Walking around Malaga', '[{"coords":{"latitude":36.71701963170518,"longitude":-4.428456299069059,"altitude":100,"accuracy":150,"altitudeAccuracy":80,"heading":134,"speed":7},"timestamp":1365973452400},{"coords":{"latitude":36.71700243127462,"longitude":-4.427855484249726,"altitude":100,"accuracy":150,"altitudeAccuracy":80,"heading":134,"speed":7},"timestamp":1365973462405},{"coords":{"latitude":36.71701963170518,"longitude":-4.4271902964140395,"altitude":100,"accuracy":150,"altitudeAccuracy":80,"heading":134,"speed":7},"timestamp":1365973472411},{"coords":{"latitude":36.7169852308402,"longitude":-4.426482193234092,"altitude":100,"accuracy":150,"altitudeAccuracy":80,"heading":134,"speed":7},"timestamp":1365973482417},{"coords":{"latitude":36.71715723501326,"longitude":-4.425495140316652,"altitude":100,"accuracy":150,"altitudeAccuracy":80,"heading":134,"speed":7},"timestamp":1365973492423},{"coords":{"latitude":36.71727763770514,"longitude":-4.425130359890636,"altitude":100,"accuracy":150,"altitudeAccuracy":80,"heading":134,"speed":7},"timestamp":1365973502429},{"coords":{"latitude":36.71757004345777,"longitude":-4.423778526547089,"altitude":100,"accuracy":150,"altitudeAccuracy":80,"heading":134,"speed":7},"timestamp":1365973512434},{"coords":{"latitude":36.71684762722407,"longitude":-4.4233922884489845,"altitude":100,"accuracy":150,"altitudeAccuracy":80,"heading":134,"speed":7},"timestamp":1365973522440},{"coords":{"latitude":36.716400413769904,"longitude":-4.423220627072018,"altitude":100,"accuracy":150,"altitudeAccuracy":80,"heading":134,"speed":7},"timestamp":1365973532446}]')
     };
     
     // bindings
@@ -68,5 +76,7 @@ $.when(pgReady, jqmReady).then(function() {
 
     $("#start-button").on("tap", startTrackingPosition);
     $("#stop-button").on("tap", stopTrackingPosition);
+    $("#clear-button").on("tap", clearLocalData);
+    $("#load-data-button").on("tap", loadSampleData);
 });
 
